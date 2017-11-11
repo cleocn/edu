1fb948644d885e3810b50fe60e0532ecdbbf60a0
zhangzg-isoft <zhangzg@isoft-info.com>
2017-11-11 21:27:06 +0800

    zzg

--- app/controller/user.js
+++ app/controller/user.js
@@ -9,19 +9,19 @@ module.exports = app => {
             ctx.body = rtn;
         }
 
-        // GET: /user
+        // GET: /user   查询所有用户
         async index() {
             const { ctx, service } = this;
             var rtn =  await service.user.findAll();
             ctx.body = rtn;
         }
-       // GET: /user/_id
+       // GET: /user/_id 根据_id查询单个用户
         async show() {
             const { ctx, service } = this;
             var rtn =  await service.user.findById(ctx.params);
             ctx.body = rtn;
         }
-       // PUT: /user/_id
+       // PUT: /user/_id  根据_id更新用户
         async update() {
             const { ctx, service } = this;
             //update(id,更新的值对)
